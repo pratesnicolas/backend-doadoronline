7238b62e3157b288e13ea631f3a50b79e5dbfc21
elieduardo <eduardoely@live.com>
2023-12-14 21:25:32 -0300

    ajuste para retornar name do enum

---
+++ src/DoadorOnline.Domain/Extensions/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace DoadorOnline.Domain;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum value)
+    {
+        Type enumType = value.GetType();
+        MemberInfo member = enumType.GetMember(value.ToString())[0];
+        var attr = member.GetCustomAttribute<DisplayAttribute>();
+        return attr != null ? attr.Name : value.ToString();
+    }
+}
